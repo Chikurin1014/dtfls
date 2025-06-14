f0942dfd46656c130b55ef236fa919d7f8bb4fe4
Chikurin1014 <chikurin1014@gmail.com>
2025-06-15 00:25:34 +0900

    :adhesive_bandage: improve ssh-agent.zsh

--- config/zsh/ssh-agent.zsh
+++ config/zsh/ssh-agent.zsh
@@ -1,5 +1,9 @@
-eval "$(ssh-agent -s)" > /dev/null
-
-for file in ~/.ssh/*.pub; do
-  ssh-add "${file%.pub}" > /dev/null
-done
+if [ $(ps aux | grep ssh-agent | grep -v grep | wc -l) -eq 0 ]; then
+    rm -f /tmp/ssh-agent.sock
+    eval $(ssh-agent -a /tmp/ssh-agent.sock) &> /dev/null
+    for file in ~/.ssh/*.pub; do
+        ssh-add "${file%.pub}" > /dev/null
+    done
+else
+    export SSH_AUTH_SOCK=/tmp/ssh-agent.sock;
+    export SSH_AGENT_PID=$(pidof ssh-agent);
