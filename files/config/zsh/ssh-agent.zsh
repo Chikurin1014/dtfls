d3dad579bf3c2e45f34c359b7bfaf4b363376eae
Chikurin1014 <chikurin1014@gmail.com>
2025-07-24 15:33:25 +0900

    :adhesive_bandage: fix ssh-agent script

--- files/config/zsh/ssh-agent.zsh
+++ files/config/zsh/ssh-agent.zsh
@@ -2,7 +2,7 @@ if [ -f ~/.ssh-agent ]; then
     source ~/.ssh-agent > /dev/null
 fi
 
-if [ -z "$SSH_AGENT_PID" ] || ! ssh-agent -k > /dev/null 2>&1; then
+if [ -z "$SSH_AGENT_PID" ] || ! kill -0 "$SSH_AGENT_PID" > /dev/null 2>&1; then
     ssh-agent > ~/.ssh-agent
     source ~/.ssh-agent > /dev/null
 fi
