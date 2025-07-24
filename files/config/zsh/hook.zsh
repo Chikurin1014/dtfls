7e386724976cc787e18fef0da6ec23fec6b2c3ca
Chikurin1014 <chikurin1014@gmail.com>
2025-07-24 19:41:45 +0900

    :adhesive_bandage: fix filematch

--- files/config/zsh/hook.zsh
+++ files/config/zsh/hook.zsh
@@ -3,7 +3,7 @@ autoload -Uz add-zsh-hook
 # コマンド実行後、ヒストリに残すかを判定
 remove_last_history_if_not_needed () {
     local last_status="$?"
-    local history_file="~/.zsh_history"
+    local history_file=~/.zsh_history
     # 直前のコマンドが失敗した場合、ヒストリから最後のエントリを削除
     if [[ "${last_status}" -ne 0 ]]; then
         # ヒストリをファイルに書き出し
